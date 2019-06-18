68e65127485d165a0d90ee714bb3071148986b8c
Devin <devinmwelch@gmail.com>
2019-06-17 20:54:55 -0400

    initialize

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log('I am ready!')
+})
+
+client.on('message', message => {
+    //Bots don't talk to bots nor links
+    if (message.content === 'hello') {
+        message.reply('hello world!')
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
